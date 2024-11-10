61c313b781d784a97328d6ae238ef3ee7e6ce2b1
Ravish Bisht <ravishbisht85.com>
2024-11-10 19:04:19 +0530

    add shad cn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button";
 
 function App() {
 
   return (
     <>
       <h1>welcome to elib dashboard</h1>
+      <Button variant="outline">Test Button</Button>
     </>
   );
 }
