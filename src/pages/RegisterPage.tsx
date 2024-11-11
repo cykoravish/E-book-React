26205d99408fd3abdb04e4d624b52aad3494c5bb
Ravish Bisht <ravishbisht85.com>
2024-11-11 09:26:59 +0530

    add register page

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
+const RegisterPage = () => {
+  return (
+    <section className="flex justify-center items-center h-screen">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="first-name">Name</Label>
+              <Input id="first-name" placeholder="Ravish" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="ravish@gmail.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Create An Account
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Already Have An Account ?
+            <Link to="/login" className="underline">
+              Sign In
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </section>
+  );
+};
+
+export default RegisterPage;
