a6f89029f8bb5e453a87a6b6ffb52cf55a84977e
Ravish Bisht <ravishbisht85.com>
2024-11-11 17:14:28 +0530

    add register functionality

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -8,9 +8,39 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { register } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
+import { LoaderPinwheel } from "lucide-react";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      console.log("login successful");
+      //redirect to dashboard
+      navigate("/dashboard/home");
+    },
+  });
+
+  const handleRegisterSubmit = () => {
+    const name = nameRef?.current?.value;
+    const email = emailRef?.current?.value;
+    const password = passwordRef?.current?.value;
+
+    if (!name || !email || !password) {
+      return alert("Please enter email and password");
+    }
+    mutation.mutate({ name, email, password });
+  };
+
   return (
     <section className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
@@ -19,16 +49,22 @@ const RegisterPage = () => {
           <CardDescription>
             Enter your information to create account
           </CardDescription>
+          {mutation.isError && (
+              <span className="text-red-500 text-sm">
+                {mutation.error.message}
+              </span>
+            )}
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
             <div className="grid gap-2">
-              <Label htmlFor="first-name">Name</Label>
-              <Input id="first-name" placeholder="Ravish" required />
+              <Label htmlFor="name">Name</Label>
+              <Input ref={nameRef} id="name" placeholder="Ravish" required />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
+                ref={emailRef}
                 id="email"
                 type="email"
                 placeholder="ravish@gmail.com"
@@ -37,10 +73,18 @@ const RegisterPage = () => {
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" required />
+              <Input ref={passwordRef} id="password" type="password" required />
             </div>
-            <Button type="submit" className="w-full">
-              Create An Account
+            <Button
+              onClick={handleRegisterSubmit}
+              type="submit"
+              className="w-full"
+              disabled={mutation.isPending}
+            >
+              {mutation.isPending && (
+                <LoaderPinwheel className="animate-spin" />
+              )}
+              <span className="ml-2">Create An Account</span>
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
