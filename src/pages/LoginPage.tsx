17275acdc89df8cafd641310d36d3a24952a1282
Ravish Bisht <ravishbisht85.com>
2024-11-11 11:27:22 +0530

    add tanstack query

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,9 +9,21 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleLoginSubmit = () => {
+    const email = emailRef?.current?.value;
+    const password = passwordRef?.current?.value;
+
+    //make api call
+    console.log("data: ", { email, password });
+  };
+
   return (
     <section className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
@@ -26,6 +38,7 @@ const LoginPage = () => {
             <Label htmlFor="email">Email</Label>
             <Input
               id="email"
+              ref={emailRef}
               type="email"
               placeholder="ravish@gmail.com"
               required
@@ -33,12 +46,14 @@ const LoginPage = () => {
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input id="password" ref={passwordRef} type="password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className="w-full">
-            <Button className="w-full">Sign In</Button>
+            <Button onClick={handleLoginSubmit} className="w-full">
+              Sign In
+            </Button>
             <div className="mt-4 text-center text-sm">
               Don't Have An Account ?
               <Link to="/auth/register" className="underline">
