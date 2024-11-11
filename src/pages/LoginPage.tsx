8278bc2064aab3a9419a7934ef84a5d001ce5693
Ravish Bisht <ravishbisht85.com>
2024-11-11 12:44:13 +0530

    add login functionality

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,19 +9,34 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { login } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
 import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      console.log("login successful");
+      //redirect to dashboard
+      navigate("/dashboard/home")
+    },
+  });
 
   const handleLoginSubmit = () => {
     const email = emailRef?.current?.value;
     const password = passwordRef?.current?.value;
 
-    //make api call
-    console.log("data: ", { email, password });
+    if (!email || !password) {
+      return alert("Please enter email and password");
+    }
+    mutation.mutate({ email, password });
   };
 
   return (
