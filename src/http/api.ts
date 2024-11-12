2aaf77926f6c7398894adb6c750973d8da8d8fb8
Ravish Bisht <ravishbisht85.com>
2024-11-12 13:59:18 +0530

    show books table

--- src/http/api.ts
+++ src/http/api.ts
@@ -9,5 +9,11 @@ const api = axios.create({
 
 export const login = async (data: { email: string; password: string }) =>
   api.post("/api/users/login", data);
-export const register = async (data: {name: string, email: string; password: string }) =>
-  api.post("/api/users/register", data);
+
+export const register = async (data: {
+  name: string;
+  email: string;
+  password: string;
+}) => api.post("/api/users/register", data);
+
+export const getBooks = async () => api.get("/api/books");
