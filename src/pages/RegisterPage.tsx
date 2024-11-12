3fe765fc9c5c2daaec30020fcdbc861a2e4392b4
Ravish Bisht <ravishbisht85.com>
2024-11-12 10:40:16 +0530

    add persist storage to register

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -9,22 +9,28 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { register } from "@/http/api";
+import useTokenStore from "@/store";
 import { useMutation } from "@tanstack/react-query";
 import { LoaderPinwheel } from "lucide-react";
 import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+  const setToken = useTokenStore((state)=>state.setToken);
+
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-  const navigate = useNavigate();
 
   // Mutations
   const mutation = useMutation({
     mutationFn: register,
-    onSuccess: () => {
+    onSuccess: (response) => {
       console.log("login successful");
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-ignore
+      setToken(response.data.accessToken)
       //redirect to dashboard
       navigate("/dashboard/home");
     },
